1022e05dafd28c37cc077aab0ba1dc98b8b2aa91
timbueschinghslu <timbuesching@stud.hslu.ch>
2024-10-31 14:34:21 +0100

    docker, train, requirements added

---
+++ train.py
@@ -0,0 +1,272 @@
+import argparse
+from dotenv import load_dotenv
+import os
+import wandb
+import datasets
+import torch
+import evaluate
+import lightning as L
+from datetime import datetime
+from lightning.pytorch.loggers import WandbLogger
+from torch.utils.data import DataLoader
+from transformers import (
+    AutoConfig,
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    get_linear_schedule_with_warmup,
+)
+
+class GLUEDataModule(L.LightningDataModule):
+    task_text_field_map = {
+        "cola": ["sentence"],
+        "sst2": ["sentence"],
+        "mrpc": ["sentence1", "sentence2"],
+        "qqp": ["question1", "question2"],
+        "stsb": ["sentence1", "sentence2"],
+        "mnli": ["premise", "hypothesis"],
+        "qnli": ["question", "sentence"],
+        "rte": ["sentence1", "sentence2"],
+        "wnli": ["sentence1", "sentence2"],
+        "ax": ["premise", "hypothesis"],
+    }
+
+    glue_task_num_labels = {
+        "cola": 2,
+        "sst2": 2,
+        "mrpc": 2,
+        "qqp": 2,
+        "stsb": 1,
+        "mnli": 3,
+        "qnli": 2,
+        "rte": 2,
+        "wnli": 2,
+        "ax": 3,
+    }
+
+    loader_columns = [
+        "datasets_idx",
+        "input_ids",
+        "token_type_ids",
+        "attention_mask",
+        "start_positions",
+        "end_positions",
+        "labels",
+    ]
+
+    def __init__(
+        self,
+        model_name_or_path: str,
+        task_name: str = "mrpc",
+        max_seq_length: int = 128,
+        train_batch_size: int = 32,
+        eval_batch_size: int = 32,
+        **kwargs,
+    ):
+        super().__init__()
+        self.model_name_or_path = model_name_or_path
+        self.task_name = task_name
+        self.max_seq_length = max_seq_length
+        self.train_batch_size = train_batch_size
+        self.eval_batch_size = eval_batch_size
+
+        self.text_fields = self.task_text_field_map[task_name]
+        self.num_labels = self.glue_task_num_labels[task_name]
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name_or_path, use_fast=True)
+
+    def setup(self, stage: str):
+        self.dataset = datasets.load_dataset("glue", self.task_name)
+
+        for split in self.dataset.keys():
+            self.dataset[split] = self.dataset[split].map(
+                self.convert_to_features,
+                batched=True,
+                remove_columns=["label"],
+            )
+            self.columns = [c for c in self.dataset[split].column_names if c in self.loader_columns]
+            self.dataset[split].set_format(type="torch", columns=self.columns)
+
+        self.eval_splits = [x for x in self.dataset.keys() if "validation" in x]
+
+    def prepare_data(self):
+        datasets.load_dataset("glue", self.task_name)
+        AutoTokenizer.from_pretrained(self.model_name_or_path, use_fast=True)
+
+    def train_dataloader(self):
+        return DataLoader(self.dataset["train"], batch_size=self.train_batch_size, shuffle=True)
+
+    def val_dataloader(self):
+        if len(self.eval_splits) == 1:
+            return DataLoader(self.dataset["validation"], batch_size=self.eval_batch_size)
+        elif len(self.eval_splits) > 1:
+            return [DataLoader(self.dataset[x], batch_size=self.eval_batch_size) for x in self.eval_splits]
+
+    def test_dataloader(self):
+        if len(self.eval_splits) == 1:
+            return DataLoader(self.dataset["test"], batch_size=self.eval_batch_size)
+        elif len(self.eval_splits) > 1:
+            return [DataLoader(self.dataset[x], batch_size=self.eval_batch_size) for x in self.eval_splits]
+
+    def convert_to_features(self, example_batch, indices=None):
+        # Either encode single sentence or sentence pairs
+        if len(self.text_fields) > 1:
+            texts_or_text_pairs = list(zip(example_batch[self.text_fields[0]], example_batch[self.text_fields[1]]))
+        else:
+            texts_or_text_pairs = example_batch[self.text_fields[0]]
+
+        # Tokenize the text/text pairs
+        features = self.tokenizer.batch_encode_plus(
+            texts_or_text_pairs, max_length=self.max_seq_length, pad_to_max_length=True, truncation=True
+        )
+
+        # Rename label to labels to make it easier to pass to model forward
+        features["labels"] = example_batch["label"]
+
+        return features
+
+class GLUETransformer(L.LightningModule):
+    class GLUETransformer(L.LightningModule):
+    def __init__(
+        self,
+        model_name_or_path: str,
+        num_labels: int,
+        task_name: str,
+        learning_rate: float = 2e-5,
+        warmup_steps: int = 0,
+        weight_decay: float = 0.0,
+        train_batch_size: int = 32,
+        eval_batch_size: int = 32,
+        eval_splits: Optional[list] = None,
+        **kwargs,
+    ):
+        super().__init__()
+
+        self.save_hyperparameters()
+
+        self.config = AutoConfig.from_pretrained(model_name_or_path, num_labels=num_labels)
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_name_or_path, config=self.config)
+        self.metric = evaluate.load(
+            "glue", self.hparams.task_name, experiment_id=datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+        )
+
+        self.validation_step_outputs = []
+
+    def forward(self, **inputs):
+        return self.model(**inputs)
+
+    def training_step(self, batch, batch_idx):
+        outputs = self(**batch)
+        loss = outputs[0]
+        self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx, dataloader_idx=0):
+        outputs = self(**batch)
+        val_loss, logits = outputs[:2]
+
+        if self.hparams.num_labels > 1:
+            preds = torch.argmax(logits, axis=1)
+        elif self.hparams.num_labels == 1:
+            preds = logits.squeeze()
+
+        labels = batch["labels"]
+        self.validation_step_outputs.append({"loss": val_loss, "preds": preds, "labels": labels})
+        return val_loss
+
+    def on_validation_epoch_end(self):
+        if self.hparams.task_name == "mnli":
+            for i, output in enumerate(self.validation_step_outputs):
+                # matched or mismatched
+                split = self.hparams.eval_splits[i].split("_")[-1]
+                preds = torch.cat([x["preds"] for x in output]).detach().cpu().numpy()
+                labels = torch.cat([x["labels"] for x in output]).detach().cpu().numpy()
+                loss = torch.stack([x["loss"] for x in output]).mean()
+                self.log(f"val_loss_{split}", loss, prog_bar=True)
+                split_metrics = {
+                    f"{k}_{split}": v for k, v in self.metric.compute(predictions=preds, references=labels).items()
+                }
+                self.log_dict(split_metrics, prog_bar=True)
+            self.validation_step_outputs.clear()
+            return loss
+
+        preds = torch.cat([x["preds"] for x in self.validation_step_outputs]).detach().cpu().numpy()
+        labels = torch.cat([x["labels"] for x in self.validation_step_outputs]).detach().cpu().numpy()
+        loss = torch.stack([x["loss"] for x in self.validation_step_outputs]).mean()
+        self.log("val_loss", loss, prog_bar=True)
+        self.log_dict(self.metric.compute(predictions=preds, references=labels), prog_bar=True)
+        self.validation_step_outputs.clear()
+
+    def configure_optimizers(self):
+        """Prepare optimizer and schedule (linear warmup and decay)"""
+        model = self.model
+        no_decay = ["bias", "LayerNorm.weight"]
+        optimizer_grouped_parameters = [
+            {
+                "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+                "weight_decay": self.hparams.weight_decay,
+            },
+            {
+                "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
+                "weight_decay": 0.0,
+            },
+        ]
+        optimizer = torch.optim.AdamW(optimizer_grouped_parameters, lr=self.hparams.learning_rate)
+
+        scheduler = get_linear_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=self.hparams.warmup_steps,
+            num_training_steps=self.trainer.estimated_stepping_batches,
+        )
+        scheduler = {"scheduler": scheduler, "interval": "step", "frequency": 1}
+        return [optimizer], [scheduler]
+
+
+if __name__ == "__main__":
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--checkpoint_dir", type=str, required=True, help="Directory to save model checkpoints")
+    parser.add_argument("--lr", type=float, default=5e-5, help="Learning rate for training")
+    parser.add_argument("--warmup_steps", type=int, default=13, help="Number of warmup steps for learning rate scheduler")
+    parser.add_argument("--weight_decay", type=float, default=0.02, help="Weight decay for optimizer")
+    parser.add_argument("--train_batch_size", type=int, default=64, help="Training batch size")
+    parser.add_argument("--task_name", type=str, default="mrpc", help="GLUE task name")
+    parser.add_argument("--model_name_or_path", type=str, default="distilbert-base-uncased", help="Model name or path")
+    args = parser.parse_args()
+
+    load_dotenv()
+    wandb_api_key = os.getenv("WANDB_API_KEY")
+
+    # Initialize Weights & Biases
+    wandb.init(project="p2_mlops", config=args)
+
+    # Prepare data module and model
+    dm = GLUEDataModule(
+        model_name_or_path=args.model_name_or_path,
+        task_name=args.task_name,
+        train_batch_size=args.train_batch_size
+    )
+    dm.setup("fit")
+
+    model = GLUETransformer(
+        model_name_or_path=args.model_name_or_path,
+        num_labels=dm.num_labels,
+        eval_splits=dm.eval_splits,
+        task_name=dm.task_name,
+        learning_rate=args.lr,
+        weight_decay=args.weight_decay,
+        warmup_steps=args.warmup_steps,
+    )
+
+    # Setup Trainer with Weights & Biases logger
+    wandb_logger = WandbLogger(project="p2_mlops")
+    trainer = L.Trainer(
+        max_epochs=3,
+        accelerator="mps",
+        devices=1,
+        logger=wandb_logger,
+        gradient_clip_val=1.0,
+        default_root_dir=args.checkpoint_dir  # Directory for model checkpoints
+    )
+
+    # Train the model
+    trainer.fit(model, datamodule=dm)
+    wandb.finish()
